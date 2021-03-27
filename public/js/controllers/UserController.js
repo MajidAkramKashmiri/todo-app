09f593d212b0a71859c508f452bdbfc251f0a34a
Majid Kashmiri <majidak0067@gmail.com>
2021-03-27 13:16:41 +0500

    feat - added userAvatar component in the datatable component  and display it in the table

---
+++ public/js/controllers/UserController.js
@@ -0,0 +1,96 @@
+app.controller("crudController", function ($scope, $http,$uibModal,UserService) {
+    $scope.userManagement = function () {
+        document.location.hash = "#!/home-ums"
+    }
+    $scope.todolist = function(){
+    document.location.hash = "#!/home-todo"
+    }
+    $scope.UserService=UserService;
+    $scope.data = {
+        username: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        address: ""
+    }
+    $scope.userDefinition = [
+        {
+            label:'UserAvatar',
+            dataType:'userAvatar',
+            data:(item)=>{
+               return item.username
+            }
+        },
+        {
+            label:'Username',
+            dataType:'text',
+            data:(item)=>{
+               return item.username
+            }
+        },
+        {
+            label:'First Name',
+            dataType:'text',
+            data:(item)=>{
+                return item.firstName
+            }
+        },
+        {
+            label:'Last Name',
+            dataType:'text',
+            data:(item)=>{
+                return item.lastName
+            }
+        }
+    ]
+    $scope.open = function (user, action) {
+        let modalInstance = $uibModal.open({
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: "html/createUserModal.html",
+            controller: "CreateUserController",
+            resolve: {
+                user: function () {
+                    return user;
+                },
+                action: function () {
+                    return action;
+                }
+            }
+        });
+        modalInstance.result.then(function() {
+            UserService.get()
+                .then(response=>{
+                    $scope.usrData=response;
+                }) 
+            }, 
+            function(){
+            }
+        );
+    };
+     
+    UserService.get()
+        .then(response=>{
+            $scope.usrData=response;
+        }) 
+    $scope.addUser = function () {
+        UserService.create($scope.data)
+            .then(response=>{
+            }) 
+        UserService.get()
+            .then(response=>{
+             $scope.usrData=response;
+            })  
+    }
+    $scope.delete = function (id) {
+        UserService.delete(id)
+            .then(response=>{
+            })  
+        UserService.get()
+            .then(response=>{
+            $scope.usrData=response;
+            })   
+    }
+});
