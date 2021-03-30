8e3cb6dff9954a6d3efe48d2239460d2b0027351
Majid Kashmiri <majidak0067@gmail.com>
2021-03-30 11:36:03 +0500

    feat - used the same component data table for todo list and updated the file according to it

--- public/js/controllers/home-todo.js
+++ public/js/controllers/home-todo.js
@@ -1,15 +1,72 @@
 app.controller("todocontroller", function ($scope, $http, $uibModal, todoListService) {
+    $scope.refreshDataTable=0; 
     $scope.userManagement = function () {
         document.location.hash = "#!/home-ums"
     }
     $scope.todolist = function () {
         document.location.hash = "#!/home-todo"
     }
+    $scope.Service = todoListService;
     $scope.list = {
         title: "",
         priority:"",
         description: ""
     };
+    $scope.ListDefinition = [
+        {
+            label:'Title',
+            dataType:'text',
+            data:(item)=>{
+                return item.title
+                console.log("todoListItem");
+            }
+        },
+        {
+            label:'Priority',
+            dataType:'text',
+            data:(item)=>{
+                return item.priority
+            }
+        },
+        {
+            label:'Description',
+            dataType:'text',
+            data:(item)=>{
+                
+                return item.description  
+            }
+        },
+        {   
+            label:'',
+            dataType: 'button',
+            action: (item, index) => 
+                {
+                    return $scope.open(item,'VIEW');
+                },
+            actionLabel:'View',
+            actionCssClass:'btn btn-success'    
+        },
+        {   
+            label:'',
+            dataType: 'button',
+            action: (item, index) => { 
+               return  $scope.open(item,'EDIT'); 
+            },
+            actionLabel:'Edit',
+            actionCssClass:'btn btn-warning'
+        },
+        {   
+            label:'',
+            dataType: 'button',
+            action: (item, index) => { 
+                $scope.refreshDataTable++;
+                return $scope.delete(item._id);
+            },
+            actionLabel:'Delete',
+            actionCssClass:'btn btn-danger'
+        }
+
+]
     $scope.open = function (list, action) {
         let modalInstance = $uibModal.open({
             ariaLabelledBy: 'modal-title',
@@ -28,6 +85,7 @@ app.controller("todocontroller", function ($scope, $http, $uibModal, todoListSer
         modalInstance.result.then(function() {
             todoListService.get()
                 .then(response=>{
+                    $scope.refreshDataTable++;
                     $scope.todoListData=response;
                 }) 
         }, 
@@ -38,7 +96,7 @@ app.controller("todocontroller", function ($scope, $http, $uibModal, todoListSer
     todoListService.get()
         .then(response=>{
             $scope.todoListData = response;
-         })
+        })
     $scope.addTodo = function () {
         todoListService.create($scope.list)
             .then(response=>{
